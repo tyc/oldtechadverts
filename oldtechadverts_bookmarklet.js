474c5041b156d3f33e20b2b12fcfbe67220a41f1
Tehn Yit Chin <tehn.yit.chin@gmail.com>
2020-12-21 22:02:47 +0100

    Cleaning up. It is parsing the url parameters

--- oldtechadverts_bookmarklet.js
+++ oldtechadverts_bookmarklet.js
@@ -1,57 +1,41 @@
-(function() {
-    alert('We are now killing it');
-  })();
-
-
+/* This javascript function will launch new window at the URL for the survey. 
+ * and pass on it the answers for the survey.
+ */
 (function() {
 
-  window.open("https://docs.google.com/forms/d/e/1FAIpQLSf9_xxtlrBYOg5tUgPGdcbBF0IzFwGRQiZ8kWCrdgXDi_Pg2A/viewform?usp=sf_link");
+  const currentURL = window.location.href.split(/[?#]/)[0];
+  console.log("currentURL="+currentURL);
 
-  var listPaperinputInput = document.getElementsByClassName('quantumWizTextinputPaperinputInputArea');
-  console.log(listPaperinputInput);
-  listPaperinputInput[0].value="Hello,World";
-  listPaperinputInput[1].value="Goodbye, World"
+  const myFormURL = new URL("https://docs.google.com/forms/d/e/1FAIpQLSf9_xxtlrBYOg5tUgPGdcbBF0IzFwGRQiZ8kWCrdgXDi_Pg2A/viewform");
+
+  /* attached the first answer to the parameter */
+  myFormURL.searchParams.append("q1",currentURL);
+
+  console.log(myFormURL.href);
   
-  var listPapertextareaInput = document.getElementsByClassName('quantumWizTextinputPapertextareaInput');
-  console.log(listPapertextareaInput);
-  listPapertextareaInput[0].value="I am in the middle";
-
-})();
+  window.open(myFormURL);
+}
+)();
 
 
 (function() {
+
+  /* parse the command line to extract the parameters for the three questions.
+   */
+  const queryString = window.location.search;
+  console.log(queryString);  
+  const urlParams = new URLSearchParams(queryString);
+  console.log(urlParams);
+  
+  const a1=urlParams.get('q1');
+  console.log(a1);
+
 
   
   var listPapertextareaInput = document.getElementsByClassName("quantumWizTextinputPapertextareaInput exportTextarea");
   
-  /*
-  console.log(listPapertextareaInput);
-  listPapertextareaInput[0].value="I am not here12";
-  listPapertextareaInput[0].text="I am not here12";
-  listPapertextareaInput[1].value="I am not here13";
-  listPapertextareaInput[1].text="I am not here13";
-  listPapertextareaInput[2].value="I am not here14";
-  listPapertextareaInput[2].text="I am not here14";
-  */
   
-  console.log(listPapertextareaInput[0]);
-	listPapertextareaInput[0].setAttribute("data-initial-value","I am not here12");
-  listPapertextareaInput[0].setAttribute("badinput","false");
-  listPapertextareaInput[0].value="I am not here12";
-  listPapertextareaInput[0].text="I am not here12";
-  console.log(listPapertextareaInput[0]);
 
-})();
-
-<div class="quantumWizTextinputPapertextareaEl modeLight freebirdFormviewerComponentsQuestionTextLong freebirdFormviewerComponentsQuestionTextTextInput freebirdThemedInput hasValue" jscontroller="pxq3x" jsaction="clickonly:KjsqPd; focus:Jt1EX; blur:fpfTEe; input:Lg5SV" jsshadow="" jsname="YPqjbf"><div class="quantumWizTextinputPapertextareaMainContent exportContent"><div class="quantumWizTextinputPapertextareaPlaceholder exportLabel" aria-hidden="true" jsname="LwH6nd">Your answer</div><div class="quantumWizTextinputPapertextareaContentArea exportContentArea"><textarea class="quantumWizTextinputPapertextareaInput exportTextarea" jsname="YPqjbf" data-rows="1" tabindex="0" aria-label="Your answer" jscontroller="gZjhIf" jsaction="input:Lg5SV;ti6hGc:XMgOHc;rcuQ6b:WYd;" dir="auto" data-initial-dir="auto" data-initial-value="I am not here12" aria-labelledby="i1" aria-describedby="i2 i3" style="height: 24px;" badinput="false"></textarea></div><div class="quantumWizTextinputPapertextareaUnderline exportUnderline"></div><div jsname="XmnwAc" class="quantumWizTextinputPapertextareaFocusUnderline exportFocusUnderline animationInitialized" style="transform-origin: 124.633px center 0px;"></div></div><div class="quantumWizTextinputPapertextareaCounterErrorHolder"><div jsname="ty6ygf" class="quantumWizTextinputPapertextareaHint exportHint"></div></div></div>
-<div class="quantumWizTextinputPapertextareaEl modeLight freebirdFormviewerComponentsQuestionTextLong freebirdFormviewerComponentsQuestionTextTextInput freebirdThemedInput hasValue" jscontroller="pxq3x" jsaction="clickonly:KjsqPd; focus:Jt1EX; blur:fpfTEe; input:Lg5SV" jsshadow="" jsname="YPqjbf"><div class="quantumWizTextinputPapertextareaMainContent exportContent"><div class="quantumWizTextinputPapertextareaPlaceholder exportLabel" aria-hidden="true" jsname="LwH6nd">Your answer</div><div class="quantumWizTextinputPapertextareaContentArea exportContentArea"><textarea class="quantumWizTextinputPapertextareaInput exportTextarea" jsname="YPqjbf" data-rows="1" tabindex="0" aria-label="Your answer" jscontroller="gZjhIf" jsaction="input:Lg5SV;ti6hGc:XMgOHc;rcuQ6b:WYd;" dir="auto" data-initial-dir="auto" data-initial-value="I am not here12" aria-labelledby="i1" aria-describedby="i2 i3" style="height: 24px;" badinput="false"></textarea></div><div class="quantumWizTextinputPapertextareaUnderline exportUnderline"></div><div jsname="XmnwAc" class="quantumWizTextinputPapertextareaFocusUnderline exportFocusUnderline animationInitialized" style="transform-origin: 151.633px center 0px;"></div></div><div class="quantumWizTextinputPapertextareaCounterErrorHolder"><div jsname="ty6ygf" class="quantumWizTextinputPapertextareaHint exportHint"></div></div></div>
-
-
-(function() {
-
-  
-  var listPapertextareaInput = document.getElementsByClassName("quantumWizTextinputPapertextareaInput exportTextarea");
-  
   console.log(listPapertextareaInput[0].className);
   console.log(listPapertextareaInput[0]);
   
