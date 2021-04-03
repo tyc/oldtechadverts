568c470212aa2cbd274cd5569bcdd374f886124d
tyc <tehn.yit.chin@gmail.com>
2021-04-03 12:00:35 +0200

    Adding to git

---
+++ tools/extract.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python
+import os
+import subprocess
+import fnmatch
+
+# extract each page of the pdf to its own individual page.
+# pdftk does the heaving lifting.
+#
+filelist = os.listdir("./")
+for filein in filelist:
+	if filein.endswith('.pdf'):
+		basename = os.path.splitext(filein)
+		print("processing "+basename[0])
+		command = "pdftk "+str(filein)+" burst output "+basename[0]+"_pg%02d.pdf"
+		# print(command)
+		os.system(command)
+
+reference_filename = basename[0]
+
+# convert each of the page to png
+filelist = fnmatch.filter(os.listdir("./"), reference_filename+"_pg*.pdf")
+for filein in filelist:
+	if filein.endswith('.pdf'):
+
+		basename = os.path.splitext(filein)
+		command = "pdftoppm -png -rx 300 -ry 300 "+str(filein)+" "+basename[0]
+		print(command)
+		os.system(command)
+		os.system("rm "+str(filein))
