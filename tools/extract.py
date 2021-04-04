34b158694ae8763585715f34a38249030858df20
tyc <tehn.yit.chin@gmail.com>
2021-04-04 23:23:54 +0200

    Changed the resolution. Show progress

--- tools/extract.py
+++ tools/extract.py
@@ -20,11 +20,17 @@ reference_filename = basename[0]
 
 # convert each of the page to png
 filelist = fnmatch.filter(os.listdir("./"), reference_filename+"_pg*.pdf")
+num_of_png = len(filelist)
+num_count = 0
 for filein in filelist:
 	if filein.endswith('.pdf'):
 
+		num_count += 1
+
 		basename = os.path.splitext(filein)
-		command = "pdftoppm -png -rx 300 -ry 300 "+str(filein)+" Screenshot_"+str(date.today())+"_"+basename[0]
-		print(command)
+		fileout = "Screenshot_"+str(date.today())+"_"+basename[0]
+
+		command = "pdftoppm -png -rx 300 -ry 300 "+str(filein)+" -scale-to 800 "+str(fileout)
+		print("processing "+str(num_count)+"/"+str(num_of_png)+": "+str(filein))
 		os.system(command)
 		os.system("rm "+str(filein))
